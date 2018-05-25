952626980f557cca9bfe3af5c9fea9385e7f4daa
killemall <bugurtoff@gmail.com>
2018-05-26 01:37:58 +0300

    new search algorithm

--- functions/check.php
+++ functions/check.php
@@ -17,16 +17,19 @@
         include_once('checker.php');
         
         //[TESTER]
+        echo("Введенный код: $code<br>Оригинал:<br>");
+        echo "1 столбец: ".$quebec[0]."<br> 2 столбец: ".$quebec[1]."<br>Таблица проверенных кодов: ".$whiskey[0]."<br>Количество проверок: ".$whiskey[1];
+
         // print_r($original);
         // echo("<br>");
         // print_r($_checked);
         // echo("<br>");
         // print_r($checked);
 
-        // echo("<br>"."count: ".$count."<br>"."exist1: ".$exist1."<br>"."exist2: ".$exist2."<br>"."exist3: ".$exist3."<br>");
+        //echo("<br>"."count: ".$count."<br>"."exist1: ".$exist1."<br>"."exist2: ".$exist2."<br>"."exist3: ".$exist3."<br>");
 
         // Код в БД существует и он проверялся
-        if($exist1 != 0 && $exist2 != 0 && $exist3 != 0 && $count > 0){
+        if($quebec[0] && $quebec[1] && $whiskey[0]){
             $count++;
             $_SESSION['response'] = 2;
             // Если код проверялся больше 1 раза - увеличить счетчик повторных проверок
@@ -36,7 +39,7 @@
             //echo("Код в БД существует и он проверялся");
         }
         // Код проверялся только 1 раз и в данный момент проверяется повторно
-        else if($exist1 != 0 && $exist2 != 0 && $exist3 != 0 && $count <= 1){
+        else if($quebec[0] && $quebec[1] && !$whiskey[0]){
             $_SESSION['response'] = 2;
             $sql = mysqli_query($conn, "INSERT INTO checked (code, count_of_query, phoneNumber, geo, comment, created_at) VALUES ('$code', '1', '$phone', '$region_ru', 'Проверен повторно', '$date')");
             echo'<meta http-equiv = "Refresh" content = "0 ; URL = ../index.php">';
@@ -45,7 +48,7 @@
         }
 
         // Код в БД существует и не проверялся [Оригинал]
-        if($exist1 != 0 && $exist2 == 0 && $count == 0){
+        if($quebec[0] && !$quebec[1]){
             $_SESSION['response'] = 1;
             $sql = mysqli_query($conn, "UPDATE code SET compare = '$code', isChecked = '1', phoneNumber = '$phone', geo = '$region_ru', comment = 'Оригинал', created_at = '$date' WHERE code_number = '$code'");
             echo'<meta http-equiv = "Refresh" content = "0 ; URL = ../index.php">';
@@ -54,7 +57,7 @@
         }
 
         // Код в БД не существует
-        if($exist1 == 0){
+        if(!$quebec[0]){
             $_SESSION['response'] = 3;
             $sql = mysqli_query($conn, "INSERT INTO fake (code, phoneNumber, geo, comment, created_at) VALUES ('$code', '$phone', '$region_ru', 'Контрафакт', '$date')");
             echo'<meta http-equiv = "Refresh" content = "0 ; URL = ../index.php">';
--- functions/checker.php
+++ functions/checker.php
@@ -1,20 +1,11 @@
 <?php
-    // Таблица code 1 столбец
-    $original = mysqli_query($conn, "SELECT code_number FROM code WHERE EXISTS(SELECT code_number FROM code WHERE code_number = '$code')");
-    // Таблица code 2 столбец
-    $_checked = mysqli_query($conn, "SELECT compare FROM code WHERE EXISTS(SELECT code_number FROM code WHERE compare = '$code')");
-    // Таблица checked столбец code
-    $checked = mysqli_query($conn, "SELECT code FROM checked WHERE EXISTS(SELECT code_number FROM code WHERE compare = '$code')");
-
-    // Проверка существования ячейки
-    $exist1 = $original->num_rows;
-    $exist2 = $_checked->num_rows;
-    $exist3 = $checked->num_rows;
-
-    // Таблица checked - забрать все
-    $mttt = mysqli_query($conn, "SELECT * FROM checked WHERE code = '$code'");
-    // Достать ячейку с количеством проверок кода в переменную
-    $countcheck = mysqli_fetch_row($mttt);
-    $count = $countcheck['2'];
+    // Таблица code 
+    $original = mysqli_query($conn, "SELECT code_number, compare FROM code WHERE code_number = '$code'");
+    $quebec = mysqli_fetch_array($original);
+    // Таблица checked
+    $checked = mysqli_query($conn, "SELECT code, count_of_query FROM checked WHERE code = '$code'");
+    $whiskey = mysqli_fetch_array($checked);
+    // Количество проверок
+    $count = $whiskey[1];
     $count = (int)$count;
 ?>
