6d3c6ca7b96ec864f50f9a86179f8b5e60ced8a1
killemall <bugurtoff@gmail.com>
2018-06-05 02:28:15 +0300

    creator update

--- functions/creator.php
+++ functions/creator.php
@@ -1,35 +1,55 @@
-<?php
-  include_once('connect.php');
+<?
+// Страница регистрации нового пользователя
 
-  if(isset($_POST['do_signup'])){
-        $log = $_POST['login'];
-        $password = $_POST['password'];
-        $new = password_hash($password, PASSWORD_DEFAULT);
-        $sql = mysqli_query($conn, "INSERT INTO `users`(`login`, `password`) VALUES('$log', '$new')");
-        if (!$sql) {
-            printf("Error: %s\n", mysqli_error($conn));
-            exit();
-            }
-        echo($login . "<br>" . $new);
-        unset($login);
-        unset($password);
-  }
+// Соединямся с БД
+include "connect.php";
+if(isset($_POST['submit']))
+{
+    $err = [];
+
+    // проверям логин
+    if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login']))
+    {
+        $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+    }
+
+    if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30)
+    {
+        $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+    }
+
+    // проверяем, не сущестует ли пользователя с таким именем
+    $query = mysqli_query($conn, "SELECT id FROM users WHERE user_login='".mysqli_real_escape_string($conn, $_POST['login'])."'");
+    if(mysqli_num_rows($query) > 0)
+    {
+        $err[] = "Пользователь с таким логином уже существует в базе данных";
+    }
+
+    // Если нет ошибок, то добавляем в БД нового пользователя
+    if(count($err) == 0)
+    {
+
+        $login = $_POST['login'];
+
+        // Убераем лишние пробелы и делаем двойное хеширование
+        $password = md5(md5(trim($_POST['password'])));
+
+        mysqli_query($conn, "INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
+        header("Location: signin.php"); exit();
+    }
+    else
+    {
+        print "<b>При регистрации произошли следующие ошибки:</b><br>";
+        foreach($err AS $error)
+        {
+            print $error."<br>";
+        }
+    }
+}
 ?>
 
-<form action="creator.php" method="POST">
-
-<p>
-  <p><strong>Login: </strong></p>
-  <input type="text" name="login">
-</p>
-
-<p>
-  <p><strong>Password: </strong></p>
-  <input type="password" name="password">
-</p>
-
-<p>
-  <button type="submit" name="do_signup">Создать</button>
-</p>
-
+<form method="POST">
+Логин:  <input name="login" type="text" required><br><br>
+Пароль: <input name="password" type="password" required><br>
+<input name="submit" type="submit" value="Зарегистрироваться">
 </form>
